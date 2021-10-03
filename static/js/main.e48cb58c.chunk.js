333152f37bd660c23b243c00863bc0ceeba50265
Vitalii Pikozh <wisart@gmail.com>
2021-10-03 23:33:40 +0300

    Updates

--- static/js/main.e48cb58c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{9:function(e,c,n){"use strict";n.r(c);var t=n(3),o=n.n(t),r=n(0);function s(){return Object(r.jsx)("div",{children:"..."})}var i=Object(r.jsx)("span",{children:"Hello"}),j=Object(r.jsx)("span",{children:"world!"}),l=Object(r.jsxs)("div",{children:[i,j]});console.log(l);o.a.render(Object(r.jsx)(s,{}),document.querySelector("#root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.e48cb58c.chunk.js.map
