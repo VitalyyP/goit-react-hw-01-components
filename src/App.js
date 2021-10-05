423f84a22b06020c931bac731c66cc9229da4bdb
Vitalii Pikozh <wisart@gmail.com>
2021-10-05 22:07:02 +0300

    removed all .jsons to json folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Container from "./components/Container";
 import Profile from "./components/Profile";
-import user from "./user.json";
+import user from "./jsons/user.json";
 import Statistics from "./components/Statistics";
-import statisticalData from "./statistical-data.json";
+import statisticalData from "./jsons/statistical-data.json";
 import FriendList from "./components/FriendList";
-import friends from "./friends.json";
+import friends from "./jsons/friends.json";
 import TransactionHistory from "./components/TransactionHistory";
-import transactions from "./transactions.json";
+import transactions from "./jsons/transactions.json";
 
 const { avatar, name, tag, location, stats } = user;
 const { followers, views, likes } = stats;
